801981e2c9b0b5772788e78e096900c09c95573f
hauleguitare <alexander.dutyone@gmail.com>
2024-10-22 18:37:57 +0700

    update: update WebApi

---
+++ src/WebApi/Bootstraps/MonolithModularNetAuthBootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MonolithModularNET.Auth;
+
+namespace WebApi.Bootstraps;
+
+internal static class MonolithModularNetAuthBootstrapper
+{
+    internal static IServiceCollection AddMonolithModularNetAuthBootstrapper(this IServiceCollection services,
+        IConfiguration configuration, IWebHostEnvironment environment)
+    {
+        services.AddMonolithModularNetAuth(opts =>
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            opts.UseNpgsql(connectionString);
+
+            if (environment.IsDevelopment())
+            {
+                opts.EnableSensitiveDataLogging();
+                opts.EnableDetailedErrors();
+            }
+        });
+        
+        return services;
+    }
+}
