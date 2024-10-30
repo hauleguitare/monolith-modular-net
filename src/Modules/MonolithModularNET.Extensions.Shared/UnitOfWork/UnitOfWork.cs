74e3a6b125a50b6d958203169e7855eb0e890c3d
hauleguitare <alexander.dutyone@gmail.com>
2024-10-30 16:12:20 +0700

    add unit of work & repository

---
+++ src/Modules/MonolithModularNET.Extensions.Shared/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,73 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using MonolithModularNET.Extensions.Abstractions;
+
+namespace MonolithModularNET.Extensions.Shared.UnitOfWork;
+
+public abstract class UnitOfWork<TContext>(TContext context) : IUnitOfWork<TContext, IDbContextTransaction>
+    where TContext : DbContext
+{
+    private bool _disposed = false;
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+        {
+            if (disposing)
+            {
+                Context.Dispose();
+            }
+        }
+        _disposed = true;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    public virtual TContext Context { get; set; } = context;
+
+    public virtual bool SaveChanges()
+    {
+        return Context.SaveChanges() > 0;
+    }
+
+
+    public virtual async Task<bool> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return await Context.SaveChangesAsync(cancellationToken) > 0;
+    }
+
+    public virtual IDbContextTransaction BeginTransaction()
+    {
+        return Context.Database.BeginTransaction();
+    }
+
+    public virtual Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        return Context.Database.BeginTransactionAsync(cancellationToken);
+    }
+
+    public virtual void Commit()
+    {
+        Context.Database.CommitTransaction();
+    }
+
+    public virtual async Task CommitAsync(CancellationToken cancellationToken = default)
+    {
+        await Context.Database.CommitTransactionAsync(cancellationToken);
+
+    }
+
+    public virtual void Rollback()
+    {
+        Context.Database.RollbackTransaction();
+    }
+
+    public virtual async Task RollbackAsync(CancellationToken cancellationToken = default)
+    {
+        await Context.Database.RollbackTransactionAsync(cancellationToken);
+    }
+}
